672f68f2d7a14d80213297f4f5239a12f45971e7
Sunny <sunnyjaiswal1995@gmail.com>
2026-02-22 16:33:13 +0530

    Assignment 3

---
+++ Assignment 3/Task 1.py
@@ -0,0 +1,9 @@
+# Task 1: Calculate Factorial Using a Function
+def factorial(n):
+    if n == 0 or (n == 1):
+        return 1
+    else:
+        return (n * factorial(n-1))
+n = int(input('Enter a number: '))
+Factorial = factorial(n)
+print(f'Factorial of {n} is: {Factorial}')
---
+++ Assignment 3/Task 2.py
@@ -0,0 +1,6 @@
+# Task 2: Using the Math Module for Calculations
+import math
+n = int(input('Enter a number: '))
+print('Square Root: ', math.sqrt(n))
+print(f'Logarithm: {math.log(n)}')
+print(f' Sine: {math.sin(n)}')
