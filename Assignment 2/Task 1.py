74669118963c8ff16147fed061b39e807edaf9cc
SunnyKrJaiswal <49545882+SunnyKrJaiswal@users.noreply.github.com>
2026-02-22 14:50:13 +0530

    Update Task 1.py

--- Assignment 2/Task 1.py
+++ Assignment 2/Task 1.py
@@ -1,6 +1,6 @@
 # Task 1: Check if a Number is Even or Odd
 n = int(input("Enter a number: "))
 if n%2==0:
-    print(n, "is ad even Number")
+    print(n, "is an even Number")
 else:
     print(n, "is an odd Number")
