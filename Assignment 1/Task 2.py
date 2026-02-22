0a9c8e3a1212d058457401d8639aed51a0c71521
Sunny <sunnyjaiswal1995@gmail.com>
2026-02-22 14:42:53 +0530

    renaming

---
+++ Assignment 1/Task 2.py
@@ -0,0 +1,4 @@
+# Task 2: Create a Personalized Greeting
+f_Name = input('Enter your first name: ')
+l_Name = input('Enter your last name: ')
+print(f'Hello, {f_Name}  {l_Name}! Welcome to Python Program' )
--- Task 1.py
+++
@@ -1,8 +0,0 @@
-#Task 1: Perform Basic Mathematical Operations
-
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
-print("Addition: ", num1 + num2)
-print('Subtraction: ', num1 - num2)
-print('Multiplication: ', num1 * num2)
-print('Division: ', num1 / num2)
